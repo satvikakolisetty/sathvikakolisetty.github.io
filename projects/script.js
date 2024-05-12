4a7a768971c62e8e6d0cafac1fee0f8831f63f90
Sathvika Kolisetty <60738035+satvikakolisetty@users.noreply.github.com>
2024-05-12 10:51:00 -0400

    Update script.js

--- projects/script.js
+++ projects/script.js
@@ -43,26 +43,25 @@ function getProjects() {
 function showProjects(projects) {
     let projectsContainer = document.querySelector(".work .box-container");
     let projectsHTML = "";
-    projects.forEach(project => {
-        projectsHTML += `
-        <div class="grid-item ${project.category}">
+    // Inside the showProjects() function
+projectsHTML += `
+    <div class="grid-item ${project.category}">
         <div class="box tilt" style="width: 380px; margin: 1rem">
-      <img draggable="false" src="./assets/images/projects/${project.image}.png" alt="project" />
-      <div class="content">
-        <div class="tag">
-        <h3>${project.name}</h3>
+            <img draggable="false" src="https://satvikakolisetty.github.io/sathvikakolisetty.github.io/assets/images/projects/${project.image}.png" alt="project" />
+            <div class="content">
+                <div class="tag">
+                    <h3>${project.name}</h3>
+                </div>
+                <div class="desc">
+                    <p>${project.desc}</p>
+                    <div class="btns">
+                        <a href="${project.links.code}" class="btn" target="_blank">Code <i class="fas fa-code"></i></a>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="desc">
-          <p>${project.desc}</p>
-          <div class="btns">
-            
-            <a href="${project.links.code}" class="btn" target="_blank">Code <i class="fas fa-code"></i></a>
-          </div>
-        </div>
-      </div>
-    </div>
-    </div>`
-    });
+    </div>`;
+
     projectsContainer.innerHTML = projectsHTML;
 
     // vanilla tilt.js

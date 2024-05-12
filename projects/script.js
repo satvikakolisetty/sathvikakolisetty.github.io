b96c8eec36ba9f774c3d58e7afeb97a7a8ccc1ac
Sathvika Kolisetty <60738035+satvikakolisetty@users.noreply.github.com>
2024-05-12 14:39:27 -0400

    Update script.js

--- projects/script.js
+++ projects/script.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
-
     $('#menu').click(function () {
         $(this).toggleClass('fa-times');
         $('.navbar').toggleClass('nav-toggle');
@@ -28,89 +27,6 @@ document.addEventListener('visibilitychange',
             $("#favicon").attr("href", "./assets/images/favhand.png");
         }
     });
-
-
-// fetch projects start
-function getProjects() {
-    return fetch("projects.json")
-        .then(response => response.json())
-        .then(data => {
-            return data
-        });
-}
-
-
-function showProjects(projects) {
-    let projectsContainer = document.querySelector(".work .box-container");
-    let projectsHTML = "";
-
-    projects.forEach(project => {
-        // Concatenate the HTML for each project inside the loop
-        projectsHTML += `
-            <div class="grid-item ${project.category}">
-                <div class="box tilt" style="width: 380px; margin: 1rem">
-                    <img draggable="false" src="https://satvikakolisetty.github.io/sathvikakolisetty.github.io/assets/images/projects/${project.image}.png" alt="project" />
-                    <div class="content">
-                        <div class="tag">
-                            <h3>${project.name}</h3>
-                        </div>
-                        <div class="desc">
-                            <p>${project.desc}</p>
-                            <div class="btns">
-                                <a href="${project.links.code}" class="btn" target="_blank">Code <i class="fas fa-code"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>`;
-    });
-
-
-    // The rest of your code remains unchanged...
-}
-
-
-    projectsContainer.innerHTML = projectsHTML;
-
-    // vanilla tilt.js
-    // VanillaTilt.init(document.querySelectorAll(".tilt"), {
-    //     max: 20,
-    // });
-    // // vanilla tilt.js  
-
-    // /* ===== SCROLL REVEAL ANIMATION ===== */
-    // const srtop = ScrollReveal({
-    //     origin: 'bottom',
-    //     distance: '80px',
-    //     duration: 1000,
-    //     reset: true
-    // });
-
-    // /* SCROLL PROJECTS */
-    // srtop.reveal('.work .box', { interval: 200 });
-
-    // isotope filter products
-    var $grid = $('.box-container').isotope({
-        itemSelector: '.grid-item',
-        layoutMode: 'fitRows',
-        masonry: {
-            columnWidth: 200
-        }
-    });
-
-    // filter items on button click
-    $('.button-group').on('click', 'button', function () {
-        $('.button-group').find('.is-checked').removeClass('is-checked');
-        $(this).addClass('is-checked');
-        var filterValue = $(this).attr('data-filter');
-        $grid.isotope({ filter: filterValue });
-    });
-}
-
-getProjects().then(data => {
-    showProjects(data);
-})
-// fetch projects end
 
 // Start of Tawk.to Live Chat
 var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
